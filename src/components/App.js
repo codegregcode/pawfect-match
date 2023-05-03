5b41ae7e4a2c34f00f3e34104458e268c600f317
Greg Cain <codegregcode@gmail.com>
2023-05-03 19:54:49 +0100

    starts adding questions console logs axios response

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PersonQuestionnaire from './PersonQuestionnaire';
 import '../styles/app.css';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Pawfect Match</h1>
+      <PersonQuestionnaire />
     </div>
   );
 };
