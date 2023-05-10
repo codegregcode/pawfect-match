be99b1de1d96f3d4741fe89e2c5d48d64dff1eb7
Greg Cain <codegregcode@gmail.com>
2023-05-10 20:42:36 +0100

    deletes PersonQuestionnaire component and test

--- src/__tests__/PersonQuestionnaire.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-import PersonQuestionnaire from "../components/PersonQuestionnaire";
-
-describe("PersonQuestionnaire", () => {
-  it("renders correctly", () => {
-    render(<PersonQuestionnaire />);
-  });
-});
