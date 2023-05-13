4ebd6d8be6d86722f5eec2f02bedb9f589cc52bd
Greg Cain <codegregcode@gmail.com>
2023-05-13 09:28:15 +0100

    merges

--- src/components/LoginSignup.js
+++ src/components/LoginSignup.js
@@ -1,24 +1,60 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from "react";
+import React, { useState } from "react";
 import "../styles/login-signup.css";
+import { auth } from "../firebase";
 
 const LoginSignup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const { user } = userCredential;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const { user } = userCredential;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="loginsignup-container">
       <h2>Login/Signup!</h2>
       <div>
         <label htmlFor="email">Email:</label>
-        <input type="email" />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="password">Password:</label>
-        <input type="password" />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <div>
-        <button type="button" className="btn-login">
+        <button className="btn-login" type="button" onClick={handleSignup}>
           Sign up
         </button>
-        <button type="button" className="btn-login">
+        <button className="btn-login" type="button" onClick={handleLogin}>
           Login
         </button>
       </div>
