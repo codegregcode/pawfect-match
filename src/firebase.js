17c253cb9fea4fcc0cb927cb15e882915249de0f
Abi Turner <turner.abi@gmail.com>
2023-05-15 18:54:43 +0100

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -20,14 +20,14 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "ADD CONFIG DETAILS",
-  authDomain: "ADD CONFIG DETAILS",
-  databaseURL: "ADD CONFIG DETAILS",
-  projectId: "ADD CONFIG DETAILS",
-  storageBucket: "ADD CONFIG DETAILS",
-  messagingSenderId: "ADD CONFIG DETAILS",
-  appId: "ADD CONFIG DETAILS",
-  measurementId: "ADD CONFIG DETAILS",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
