a13061760118c36218a305802a2d04519b38956c
Greg Cain <codegregcode@gmail.com>
2023-05-15 18:22:46 +0100

    adds env variables for firebaseConfig

--- src/firebase.js
+++ src/firebase.js
@@ -20,14 +20,14 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "ADD CONFIG DETAILS",
-  authDomain: "ADD CONFIG DETAILS",
-  databaseURL: "ADD CONFIG DETAILS",
-  projectId: "ADD CONFIG DETAILS",
-  storageBucket: "ADD CONFIG DETAILS",
-  messagingSenderId: "ADD CONFIG DETAILS",
-  appId: "ADD CONFIG DETAILS",
-  measurementId: "ADD CONFIG DETAILS",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
