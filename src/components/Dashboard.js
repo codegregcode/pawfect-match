465d11ab8eb273014af67db5ec6de9b172065621
Abi Turner <turner.abi@gmail.com>
2023-05-16 21:16:56 +0100

    rendering to dashboard favourites

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -22,7 +22,21 @@ const Dashboard = () => {
       <h2>Dashboard</h2>
       <ul>
         {favorites.map((breed) => (
-          <div key={breed.uid}>{breed.breed.name}</div>
+          <div key={breed.breed.name}>
+            <img src={breed.breed.image_link} alt={breed.breed.name} />
+            <h3>{breed.breed.name}</h3>
+            <div className="favourite-breeds">
+              <p>Energy: {breed.breed.energy}</p>
+              <p>Grooming: {breed.breed.grooming}</p>
+              <p>Shedding: {breed.breed.shedding}</p>
+              <p>Barking: {breed.breed.barking}</p>
+              <p>Easy to train: {breed.breed.trainability}</p>
+              <p>Good With Children: {breed.breed.good_with_other_children}</p>
+              <p>Good With Other Dogs: {breed.breed.good_with_other_dogs}</p>
+              <p>Good With Strangers: {breed.breed.good_with_strangers}</p>
+              <p>Max Size: {breed.breed.max_height_male}</p>
+            </div>
+          </div>
         ))}
       </ul>
     </div>
