f4bff18cb2a5e5c899959d9d77233c0e25bbb4af
Greg Cain <codegregcode@gmail.com>
2023-05-19 22:44:06 +0100

    fixes Navbar tests and adds test ids to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,22 +7,22 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <Link to="/">
-        <button className="home" type="button">
+        <button data-testid="home-btn" className="home" type="button">
           Home
         </button>
       </Link>
       <Link to="/dashboard">
-        <button className="dashboard" type="button">
+        <button data-testid="dashboard-btn" className="dashboard" type="button">
           Dashboard
         </button>
       </Link>
       <Link to="/login">
-        <button className="navbar-button" type="submit">
+        <button data-testid="login-btn" className="navbar-button" type="submit">
           Login/SignUp!
         </button>
       </Link>
       <Link to="/">
-        <button type="button" onClick={logout}>
+        <button data-testid="signout-btn" type="button" onClick={logout}>
           Sign Out
         </button>
       </Link>
