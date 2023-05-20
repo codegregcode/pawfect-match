a4c3ffdaf69a027bd72373f108feef1106fca98f
Greg Cain <codegregcode@gmail.com>
2023-05-20 17:22:44 +0100

    adds tests for Login

---
+++ src/__tests__/Login.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Login from "../components/Login";
+
+describe("Login", () => {
+  let errorSpy;
+
+  beforeAll(() => {
+    errorSpy = jest.spyOn(console, "error").mockImplementation(() => {});
+  });
+
+  afterAll(() => {
+    errorSpy.mockRestore();
+  });
+
+  it("should render the login form", () => {
+    const { getByPlaceholderText, getByText, getByTestId } = render(
+      <Router>
+        <Login />
+      </Router>
+    );
+
+    const emailInput = getByPlaceholderText("E-mail Address");
+    const passwordInput = getByPlaceholderText("Password");
+    const loginButton = getByTestId("login-btn");
+    const loginWithGoogleButton = getByText("Login with Google");
+    const forgotPasswordLink = getByText("Forgot Password");
+    const registerLink = getByText("Register");
+    const resetPasswordLink = getByText("here");
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+    expect(loginWithGoogleButton).toBeInTheDocument();
+    expect(forgotPasswordLink).toBeInTheDocument();
+    expect(registerLink).toBeInTheDocument();
+    expect(resetPasswordLink).toBeInTheDocument();
+  });
+});
