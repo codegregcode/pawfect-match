70b1c59a2c3ec226857a0694d3ef3ecd290ccc4d
Abi Turner <turner.abi@gmail.com>
2023-05-22 15:59:04 +0100

    corrected styling for breed card again

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -17,25 +17,29 @@ const Dashboard = () => {
   return (
     <div className="fave-breeds-container">
       <h2>Dashboard</h2>
-      <ul>
-        {favorites.map((breed) => (
-          <div key={breed.breed.name} className="fav-breed-card">
-            <img src={breed.breed.image_link} alt={breed.breed.name} />
-            <h3>{breed.breed.name}</h3>
-            <div className="fave-breed-details">
-              <p>Energy: {breed.breed.energy}</p>
-              <p>Grooming: {breed.breed.grooming}</p>
-              <p>Shedding: {breed.breed.shedding}</p>
-              <p>Barking: {breed.breed.barking}</p>
-              <p>Easy to train: {breed.breed.trainability}</p>
-              <p>Good With Children: {breed.breed.good_with_other_children}</p>
-              <p>Good With Other Dogs: {breed.breed.good_with_other_dogs}</p>
-              <p>Good With Strangers: {breed.breed.good_with_strangers}</p>
-              <p>Max Size: {breed.breed.max_height_male}</p>
+      <div className="fav-breed-cards">
+        <ul>
+          {favorites.map((breed) => (
+            <div key={breed.breed.name} className="fav-breed-card">
+              <img src={breed.breed.image_link} alt={breed.breed.name} />
+              <h3>{breed.breed.name}</h3>
+              <div className="fav-breed-details">
+                <p>Energy: {breed.breed.energy}</p>
+                <p>Grooming: {breed.breed.grooming}</p>
+                <p>Shedding: {breed.breed.shedding}</p>
+                <p>Barking: {breed.breed.barking}</p>
+                <p>Easy to train: {breed.breed.trainability}</p>
+                <p>
+                  Good With Children: {breed.breed.good_with_other_children}
+                </p>
+                <p>Good With Other Dogs: {breed.breed.good_with_other_dogs}</p>
+                <p>Good With Strangers: {breed.breed.good_with_strangers}</p>
+                <p>Max Size: {breed.breed.max_height_male}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </ul>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
