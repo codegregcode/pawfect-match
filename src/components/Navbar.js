ebfda5e310091ea23910c1fce05ef83fbac22cfd
Greg Cain <codegregcode@gmail.com>
2023-05-24 15:35:57 +0100

    writes code so login, dashboard, signout btns render conditionally passes tests, but console.error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { logout } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { logout, auth } from "../firebase";
 import "../styles/navbar.css";
 import logo from "../images/logo.png";
 
 const Navbar = () => {
+  const useGetCurrentUser = () => {
+    const u = useAuthState(auth)[0];
+    return u;
+  };
+  const user = useGetCurrentUser();
+
   return (
     <div className="navbar-container">
       <img src={logo} alt="site logo" id="logo" />
@@ -13,26 +20,40 @@ const Navbar = () => {
           Home
         </button>
       </Link>
-      <Link to="/dashboard">
-        <button data-testid="dashboard-btn" className="dashboard" type="button">
-          Dashboard
-        </button>
-      </Link>
-      <Link to="/login">
-        <button data-testid="login-btn" className="navbar-button" type="submit">
-          Login/SignUp!
-        </button>
-      </Link>
-      <Link to="/">
-        <button
-          data-testid="signout-btn"
-          className="navbar-button"
-          type="button"
-          onClick={logout}
-        >
-          Sign Out
-        </button>
-      </Link>
+      {user ? (
+        <Link to="/dashboard">
+          <button
+            data-testid="dashboard-btn"
+            className="dashboard"
+            type="button"
+          >
+            Dashboard
+          </button>
+        </Link>
+      ) : null}
+      {user ? null : (
+        <Link to="/login">
+          <button
+            data-testid="login-btn"
+            className="navbar-button"
+            type="submit"
+          >
+            Login/SignUp!
+          </button>
+        </Link>
+      )}
+      {user ? (
+        <Link to="/">
+          <button
+            data-testid="signout-btn"
+            className="navbar-button"
+            type="button"
+            onClick={logout}
+          >
+            Sign Out
+          </button>
+        </Link>
+      ) : null}
     </div>
   );
 };
