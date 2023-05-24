73e56bf0e7fcf2f78de9ee5e42a210455eb6d85d
Greg Cain <codegregcode@gmail.com>
2023-05-24 12:54:50 +0100

    sets user uid as doc id of user

--- src/firebase.js
+++ src/firebase.js
@@ -17,6 +17,8 @@ import {
   collection,
   where,
   addDoc,
+  setDoc,
+  doc,
 } from "firebase/firestore";
 import { toast } from "react-toastify";
 
@@ -43,7 +45,7 @@ const signInWithGoogle = async () => {
     const q = query(collection(db, "users"), where("uid", "==", user.uid));
     const docs = await getDocs(q);
     if (docs.docs.length === 0) {
-      await addDoc(collection(db, "users"), {
+      await setDoc(doc(db, "users", user.uid), {
         uid: user.uid,
         name: user.displayName,
         authProvider: "google",
@@ -67,7 +69,7 @@ const registerWithEmailAndPassword = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const { user } = res;
-    await addDoc(collection(db, "users"), {
+    await setDoc(doc(db, "users", user.uid), {
       uid: user.uid,
       name,
       authProvider: "local",
@@ -131,8 +133,8 @@ const getFaves = async () => {
       const { uid } = user;
       const q = query(collection(db, "favourites"), where("uid", "==", uid));
       const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-        favouritesData.push(doc.data());
+      querySnapshot.forEach((aDoc) => {
+        favouritesData.push(aDoc.data());
       });
     }
   } catch (error) {
